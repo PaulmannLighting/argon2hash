0d8f9dc00a06dfcb77196c7c5a73dd9fc86a72fc
Richard Neumann <neumann@paulmann.de>
2024-06-14 08:59:22 +0200

    Init logger

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ use rand_chacha::ChaCha20Rng;
 use argon2hash::{Args, PwHasher, HASH_ERROR};
 
 fn main() {
+    env_logger::init();
     let args = Args::parse();
     let mut hasher = PwHasher::<ChaCha20Rng>::default();
 
